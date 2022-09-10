deb3350c91f98f3fdae69267b0213d1f87467859
Austin Borck <austinborck98@gmail.com>
2022-09-09 18:22:59 -0600

    stubbed html and js

--- HikeTracker/src/main/resources/static/js/script.js
+++ HikeTracker/src/main/resources/static/js/script.js
@@ -3,13 +3,50 @@ window.addEventListener('load', function() {
 	init();
 })
 ////////// END OF LOADING THE WINDOW FUNCTION //////////
-function init() {
-	//TODO:
-	//- Event handlers for buttons 
+function init() { 
 	loadAllHikes();
+///////// ADDING EVENT LISTENER TO FORM //////////
+document.createHike.createBtn.addEventListener('click', function(e){
+	e.preventDefault();
+	let hike = {};
+	hike.name = createHike.name.value;
+	hike.description = createHike.description.value;
+	hike.difficulty = createHike.difficulty.value;
+	hike.latitude = createHike.lat.value;
+	hike.longitude = createHike.long.value;
+	hike.elevation = createHike.elevation.value;
+	hike.trailLength = createHike.length.value;
+	hike.dogsAllowed = createHike.dogsAllowed.value;
+	hike.imageUrl = createHike.imageUrl.value;
+	addNewHike(hike);
+	createHike.reset();
+////////// END FORM EVENT LISTENER //////////
+});
 
-
+////////// CREATE HIKE FUNCTION //////////
+function addNewHike(hike){
+	let xhr = new XMLHttpRequest();
+	xhr.open('POST', 'api/hikes');
+	xhr.onreadystatechange = function(){
+		if(xhr.readyState === 4){
+			if(xhr.status === 200 || xhr.status === 201){
+				console.log('hike created');
+			} else if(xhr.status === 400){
+				displayError('Invalid Data');
+			}else{
+				displayError('ERROR Creating hike ' + xhr.status);
+			}
+		}
+	}
+	xhr.setRequestHeader("Content-type", "application/json");
+	let hikeJSON = JSON.stringify(hike);
+	xhr.send(hikeJSON);
+	}
 }
+////////// END CREATE HIKE FUNCTION //////////
+
+
+////////// BEGINNING LOADING/DISPLAYING ALL HIKES //////////
 function loadAllHikes() {
 	let xhr = new XMLHttpRequest();
 	xhr.open('GET', 'api/hikes');
@@ -33,13 +70,23 @@ function displayHikes(hikeList) {
 	hikeListDiv.appendChild(ul);
 	for (let hike of hikeList) {
 		let li = document.createElement('li');
+		let br = document.createElement('br');
+		ul.appendChild(br);
 		li.textContent = hike.name;
+		li.style="list-style:none";
+		li.size="10px"
 		ul.appendChild(li);
 	}
 }
+////////// END LOADING/DISPLAYING ALL HIKES //////////
 
 
-
+///////// ERROR MSG FUNCTION //////////
+function displayError(msg) {
+	let hikesList = document.getElementById('hikesList');
+	hikesList.textContent = '';
+	hikesList.textContent = msg;
+	}
 
 
 
